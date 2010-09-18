1b16860801443c67385d785735492a3ed520d80b
Charles Fain Lehman <greenknight04@gmail.com>
2010-09-18 10:51:30 -0400

    added IRCLogger.rb... forgot that...

---
+++ bin/IRCLogger.rb
@@ -0,0 +1,60 @@
+class IRCLogger
+	@logFile
+	@loc
+	attr_reader :writable, :readable
+	
+	def initialize(fileName)
+		@logFile = File.new(fileName,"w+")
+		@loc = fileName
+		@writable = true
+		@readable = true
+	end
+	
+	def openW
+		@logFile = File.open(@loc,"w+")
+		@writable = true
+		@readable = true
+	end
+	
+	def openR
+		@logFile = File.open(@loc)
+		@writable = false
+		@readable = true
+	end
+	
+	def openA
+		@logFile = File.open(@loc,"a+")
+		@writable = true
+		@readable = true
+	end
+	
+	def close
+		@logFile.close
+		@writable = false
+		@readable = false
+	end
+	
+	def <<(text)
+		write text if @writable
+	end
+	
+	def write(text)
+		@logFile.write text if @writable
+	end
+	
+	def read
+		@logFile.read if @readable
+	end
+	
+	def logA(text)
+		openA
+		write text
+		close
+	end
+	
+	def logW(text)
+		openW
+		write text
+		close
+	end
+end
